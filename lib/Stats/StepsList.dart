89f2fd2da58eea9db85532a0c3560e264c7c08b9
dli <davidli012345@gmail.com>
2020-01-12 14:51:37 -0800

    fixing typos

--- lib/Stats/StepsList.dart
+++ lib/Stats/StepsList.dart
@@ -22,67 +22,66 @@ class _StepsListState extends State<StepsList> {
         ),
       ),
       Stepper(
-          steps: [
-            Step(
-              title: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  Icon(
-                    Icons.phone_android,
-                    color: Colors.red,
-                    size: 24.0,
-                    semanticLabel: 'Announcement Icon',
-                  ),
-                  Text(" Mobile First Design"),
-                ]
-              ),
-              // redo with icon and text?
-              content: Text("Committing to great user experiences on all devices and all on platforms."),
+        steps: [
+          Step(
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Icon(
+                  Icons.phone_android,
+                  color: Colors.red,
+                  size: 24.0,
+                  semanticLabel: 'Announcement Icon',
+                ),
+                Text(" Mobile First Design"),
+              ]
             ),
-            Step(
-              title: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  Icon(
-                    Icons.shop,
-                    color: Colors.red,
-                    size: 24.0,
-                    semanticLabel: 'Announcement Icon',
-                  ),
-                  Text(" High Quality Services"),
-                ]
-              ),
-              // redo with icon and text?
-              content: Text("Providing Value To All Our Customers"),
+            // redo with icon and text?
+            content: Text("Committing to great user experiences on all devices and all on platforms."),
+          ),
+          Step(
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Icon(
+                  Icons.shop,
+                  color: Colors.red,
+                  size: 24.0,
+                  semanticLabel: 'Announcement Icon',
+                ),
+                Text(" High Quality Services"),
+              ]
             ),
-            Step(
-              title: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  Icon(
-                    Icons.account_balance,
-                    color: Colors.red,
-                    size: 24.0,
-                    semanticLabel: 'Announcement Icon',
-                  ),
-                  Text(" Cheap and Affordable"),
-                ]
-              ),
-              // redo with icon and text?
-              content: Text("Providing Affordable software Development"),
+            // redo with icon and text?
+            content: Text("Providing Value To All Our Customers"),
+          ),
+          Step(
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Icon(
+                  Icons.account_balance,
+                  color: Colors.red,
+                  size: 24.0,
+                  semanticLabel: 'Announcement Icon',
+                ),
+                Text(" Cheap and Affordable"),
+              ]
             ),
-          ],
-          currentStep: _index,
-          onStepTapped: (index) {
-            setState(() {
-              _index = index;
-            });
-          },
-          controlsBuilder: (BuildContext context,
-                  {VoidCallback onStepContinue, VoidCallback onStepCancel}) =>
-              Container(),
-        ),
-      ],
+            // redo with icon and text?
+            content: Text("Providing Affordable software Development"),
+          ),
+        ],
+        currentStep: _index,
+        onStepTapped: (index) {
+          setState(() {
+            _index = index;
+          });
+        },
+        controlsBuilder: (BuildContext context,
+                {VoidCallback onStepContinue, VoidCallback onStepCancel}) =>
+            Container(),
+      ),
     ];
   }
   
