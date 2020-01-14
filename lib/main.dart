779945324a9e689d2a0bc85c4a26934eaffab55b
dli <davidli012345@gmail.com>
2020-01-13 21:13:05 -0800

    going to the second page

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,37 @@ class MyApp extends StatelessWidget  {
   }
 }
 
+class SecondPage extends StatelessWidget {
+  @override 
+  Widget build(BuildContext context) {
+    Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.centerLeft,
+              end: Alignment.centerRight,
+              colors: [
+                Color.fromRGBO(70, 70, 220, 1.0),
+                Color.fromRGBO(30, 15, 155, 1.0)
+              ]),
+        ),
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Navbar(),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    vertical: 20.0, horizontal: 40.0),
+                child: LandingPage(),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
